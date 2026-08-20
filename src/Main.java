1c55cbeebd7784ab3528d854a41c28dd61d7a0f1
khalid <cs.devlop@gmail.com>
2026-08-20 11:53:03 +0300

    Create and display game board

--- src/Main.java
+++ src/Main.java
@@ -5,5 +5,26 @@ public class Main {
 
         System.out.println("Welcome to Tic Tac Toe!");
 
-        }
+        /* ----------Create Code For Tic_Tac_Toe  Plyer------------------- */
+
+        // initialize 2D Array For board
+        char[][] board = {{'1', '2', '3'},
+                {'4', '5', '6'},
+                {'7', '8', '9'}};
+        // call method print board.
+        printBoard(board);
+
     }
+
+    /* ----------Create Methode For This Project--------------------- */
+
+    // Create Method for print board.
+    public static void printBoard(char[][] board) {
+        System.out.println(board[0][0] + " | " + board[0][1] + " | " + board[0][2]);
+        System.out.println("---------");
+        System.out.println(board[1][0] + " | " + board[1][1] + " | " + board[1][2]);
+        System.out.println("---------");
+        System.out.println(board[2][0] + " | " + board[2][1] + " | " + board[2][2]);
+    }
+
+}
