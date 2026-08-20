2902a59f9aec44a29088c3b096db4d293b671455
khalid <cs.devlop@gmail.com>
2026-08-20 13:42:54 +0300

    Create Tow- Method for print board and Player Move board.

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,14 @@
+import java.util.Scanner;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-
+        Scanner input = new Scanner(System.in);
+        System.out.println("*---------------------*");
         System.out.println("Welcome to Tic Tac Toe!");
+        System.out.println("*---------------------*");
+
 
         /* ----------Create Code For Tic_Tac_Toe  Plyer------------------- */
 
@@ -14,17 +19,27 @@ public class Main {
         // call method print board.
         printBoard(board);
 
+        // Enter Player Position
+
+
+        // call method for print Position
+        printBoard(board);
+
+
     }
 
     /* ----------Create Methode For This Project--------------------- */
 
     // Create Method for print board.
     public static void printBoard(char[][] board) {
-        System.out.println(board[0][0] + " | " + board[0][1] + " | " + board[0][2]);
-        System.out.println("---------");
-        System.out.println(board[1][0] + " | " + board[1][1] + " | " + board[1][2]);
-        System.out.println("---------");
-        System.out.println(board[2][0] + " | " + board[2][1] + " | " + board[2][2]);
+
+        System.out.println("   "+board[0][0] + " | " + board[0][1] + " | " + board[0][2]);
+        System.out.println("   ---------");
+        System.out.println("   "+board[1][0] + " | " + board[1][1] + " | " + board[1][2]);
+        System.out.println("   ---------");
+        System.out.println("   "+board[2][0] + " | " + board[2][1] + " | " + board[2][2]);
+
     }
+
 
 }
